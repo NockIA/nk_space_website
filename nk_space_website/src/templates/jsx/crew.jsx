ccdf7cc3b6f25dcb7a5c09a5998eab127c8c438e
ckevin <kevin.chaffaux@ynov.com>
2023-05-31 22:03:07 +0200

    :iphone: | add responsive background

--- nk_space_website/src/templates/jsx/crew.jsx
+++ nk_space_website/src/templates/jsx/crew.jsx
@@ -2,10 +2,18 @@ import '../css/crew.css'
 import '../css/global.css'
 import { NavBar } from '../../components/navBar/navBar'
 import { CrewComponent } from '../../components/crew_component/crewComponent'
+import { useState,useEffect } from 'react'
 
 export const Crew = () => {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => {
+          setScreenWidth(window.innerWidth);
+        };      
+        window.addEventListener('resize', handleResize);
+      }, [screenWidth]);
     return (
-        <div className='columnContainer containerCrewPage'>
+        <div className='columnContainer containerCrewPage' style={{backgroundImage:((screenWidth < 640) && `url(${"./src/assets/crew/background-crew-desktop.jpg"})`) || ((screenWidth >= 640 && screenWidth < 1000) && `url(${"./src/assets/crew/background-crew-tablet.jpg"})`) || `url(${"./src/assets/crew/background-crew-desktop.jpg"})` }}>
             <NavBar/>
             <CrewComponent crewMember = {"Douglas Hurley"}></CrewComponent>
         </div>
